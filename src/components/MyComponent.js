d53f82dee5d81fa32d428fc313a10f79de776812
DungxLas <85223326+DungxLas@users.noreply.github.com>
2024-08-15 00:01:21 +0700

    15/08/24

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,12 +3,20 @@ import UserInfor from "./UserInfor";
 import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
+  state = {
+    listUser: [
+      { id: 1, name: "shkjedbfsfnh", age: "30" },
+      { id: 2, name: "skjdhngsdnv", age: "20" },
+      { id: 3, name: "oieujfiojm", age: "25" },
+    ],
+  };
+
   render() {
     return (
       <div>
         <UserInfor />
         <br />
-        <DisplayInfor name="akimenfsakdmf" age="30" />
+        <DisplayInfor listUser={this.state.listUser} />
       </div>
     );
   }
