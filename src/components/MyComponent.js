cb595cc96fc9354c125c84cd89f67714b1e827a2
phamjhungfdungx@gmail.com <phamjhungfdungx@gmail.com>
2024-08-14 15:53:53 +0700

    cont

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,8 +7,51 @@ class MyComponent extends React.Component {
     age: 27,
   };
 
+  handleOnSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
+  handleOnChangeInput = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleClick(event) {}
+
   render() {
-    return <div>My first component</div>;
+    return (
+      <div>
+        My name is {this.state.name} and I'm {this.state.age}
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={(event) => {
+                this.handleOnChangeInput(event);
+              }}
+            />
+          </label>
+          <label>
+            Age:
+            <input
+              type="text"
+              name="age"
+              value={this.state.age}
+              onChange={(event) => {
+                this.handleOnChangeInput(event);
+              }}
+            />
+          </label>
+        </form>
+        <button onClick={this.handleClick}>Submit</button>
+      </div>
+    );
   }
 }
 
