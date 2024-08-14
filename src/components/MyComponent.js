f37292340b700180f0ea145cedc15ab62a6478ea
phamjhungfdungx@gmail.com <phamjhungfdungx@gmail.com>
2024-08-14 10:53:13 +0700

    continue

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "DungxLas",
+    address: "aisfmaf",
+    age: 27,
+  };
+
   render() {
     return <div>My first component</div>;
   }
