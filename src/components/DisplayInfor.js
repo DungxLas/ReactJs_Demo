d11bdd766d0139e5db6828e2bb0df03adec68f98
phamjhungfdungx@gmail.com <phamjhungfdungx@gmail.com>
2024-08-15 09:50:27 +0700

    cont

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,19 +1,39 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
+  state = { status: true };
+
+  toggleStatus = () => {
+    this.setState((prevState) => ({ status: !prevState.status }));
+  };
+
   render() {
     const { listUser } = this.props;
 
     return (
       <div>
-        {listUser.map((user) => {
-          return (
-            <div key={user.id}>
-              <div>My name is {user.name}</div>
-              <div>My age is {user.age}</div>
-            </div>
-          );
-        })}
+        <div>
+          <span
+            onClick={() => {
+              this.toggleStatus();
+            }}
+          >
+            {this.state.status ? "Hide list" : "Show list"}
+          </span>
+        </div>
+        {this.state.status && (
+          <div>
+            {listUser.map((user) => {
+              return (
+                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                  <div>My name is {user.name}</div>
+                  <div>My age is {user.age}</div>
+                  <hr />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,9 +5,9 @@ import DisplayInfor from "./DisplayInfor";
 class MyComponent extends React.Component {
   state = {
     listUser: [
-      { id: 1, name: "shkjedbfsfnh", age: "30" },
+      { id: 1, name: "shkjedbfsfnh", age: "16" },
       { id: 2, name: "skjdhngsdnv", age: "20" },
-      { id: 3, name: "oieujfiojm", age: "25" },
+      { id: 3, name: "oieujfiojm", age: "68" },
     ],
   };
 
