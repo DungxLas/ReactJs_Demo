2b52bcd19f1aa1bec539b87f8084ee778d91733f
phamjhungfdungx@gmail.com <phamjhungfdungx@gmail.com>
2024-08-27 10:39:17 +0700

    day8

--- src/components/Home/Homepage.js
+++ src/components/Home/Homepage.js
@@ -3,61 +3,6 @@ import pictureHomepage from "../../assets/picture-homepage.png";
 const HomePage = (props) => {
   return (
     <div className="homepage-container">
-      Home Here
-      <div class="MuiContainer-root MuiContainer-maxWidthLg">
-        <div class="MuiGrid-root homepage-banner MuiGrid-container MuiGrid-spacing-xs-5">
-          <div class="MuiGrid-root short-description MuiGrid-item">
-            <h1>
-              TẠO BÀI THI TRẮC NGHIỆM ONLINE
-              <br />
-              MIỄN PHÍ VÀ DỄ SỬ DỤNG
-            </h1>
-            <div class="short-description__subtext-box-wrapper">
-              <div class="short-description__subtext-box">
-                <img src="/images/HomePage/check-icon.svg" alt="check-icon" />
-                <p>Dễ dàng tạo hoặc upload file câu hỏi trắc nghiệm</p>
-              </div>
-              <div class="short-description__subtext-box">
-                <img src="/images/HomePage/check-icon.svg" alt="check-icon" />
-                <p>Nhiều tùy chọn trộn câu hỏi và tự động chấm bài làm</p>
-              </div>
-              <div class="short-description__subtext-box">
-                <img src="/images/HomePage/check-icon.svg" alt="check-icon" />
-                <p>
-                  Tạo bài thi lấy ngẫu nhiên từ ngân hàng câu hỏi trắc nghiệm
-                  của bạn
-                </p>
-              </div>
-              <div class="short-description__subtext-box">
-                <img src="/images/HomePage/check-icon.svg" alt="check-icon" />
-                <p>
-                  Tích hợp Chat GPT giúp bạn tự động tạo câu hỏi trắc nghiệm
-                </p>
-              </div>
-              <div class="short-description__subtext-box">
-                <img src="/images/HomePage/check-icon.svg" alt="check-icon" />
-                <p>
-                  Triển khai thi online hoặc làm bài thi online không cần cài
-                  đặt ứng dụng
-                </p>
-              </div>
-            </div>
-            <button
-              class="MuiButtonBase-root MuiButton-root MuiButton-contained register-button"
-              tabindex="0"
-              type="button"
-            >
-              <span class="MuiButton-label register-button-label">
-                <span class="register-button-icon">
-                  <img src="/icons/homepage/arrowRight.svg" alt="arrowRight" />
-                </span>
-                <span class="register-button-text">Đăng ký miễn phí</span>
-              </span>
-              <span class="MuiTouchRipple-root"></span>
-            </button>
-          </div>
-        </div>
-      </div>
       <picture>
         <source src={pictureHomepage} type="image/png" />
         <img
@@ -69,6 +14,21 @@ const HomePage = (props) => {
           }}
         />
       </picture>
+      <div className="homepage-content">
+        <div className="title-content">
+          TẠO BÀI THI TRẮC NGHIỆM ONLINE MIỄN PHÍ VÀ DỄ SỬ DỤNG
+        </div>
+        <div className="content">
+          Dễ dàng tạo hoặc upload file câu hỏi trắc nghiệm // Nhiều tùy chọn
+          trộn câu hỏi và tự động chấm bài làm // Tạo bài thi lấy ngẫu nhiên từ
+          ngân hàng câu hỏi trắc nghiệm của bạn // Tích hợp Chat GPT giúp bạn tự
+          động tạo câu hỏi trắc nghiệm // Triển khai thi online hoặc làm bài thi
+          online không cần cài đặt ứng dụng
+        </div>
+        <div className="content-button">
+          <button className="button">Đăng ký miễn phí</button>
+        </div>
+      </div>
     </div>
   );
 };
